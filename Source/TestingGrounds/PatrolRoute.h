3d5cee8a57c2c6fe3295d0efecac814d45807ffe
atabs999@gmail.com <atabs999@gmail.com>
2019-01-24 23:53:19 +0100

    PatrollingGuard class removed from the project

--- Source/TestingGrounds/PatrolRoute.h
+++ Source/TestingGrounds/PatrolRoute.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Adam Tabis
 
 #pragma once
 
@@ -6,6 +6,9 @@
 #include "Components/ActorComponent.h"
 #include "PatrolRoute.generated.h"
 
+/**
+	A "route card" to help AI choose their next waypoint	
+*/
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TESTINGGROUNDS_API UPatrolRoute : public UActorComponent
