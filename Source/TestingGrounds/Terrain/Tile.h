6141e9dd973afcfefc3fbb1dc85e58db7662ce45
atabs999@gmail.com <atabs999@gmail.com>
2019-02-23 18:31:45 +0100

    Finished procedural generation of props in Tiles Removed remaining starter content props

--- Source/TestingGrounds/Terrain/Tile.h
+++ Source/TestingGrounds/Terrain/Tile.h
@@ -15,8 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	ATile();
 
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	void PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 300.0f);
+	UFUNCTION(BlueprintCallable, Category = "Setup")
+	void PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn = 1, int32 MaxSpawn = 1, float Radius = 300.0f, float MinScale = 1, float MaxScale = 1);
 
 protected:
 	// Called when the game starts or when spawned
@@ -32,6 +32,6 @@ private:
 
 	bool FindEmptyLocation(FVector &OutLocation,float Radius);
 	
-	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint);
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale);
 
 };
